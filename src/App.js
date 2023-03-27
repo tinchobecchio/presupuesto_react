b9f9a67a26d16475154598ee2a1472b2f625c906
Martin Becchio <tincho.becchio@gmail.com>
2023-03-27 17:39:27 -0300

    agregando el componente de Pregunta

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import Pregunta from "./components/Pregunta";
 function App() {
   return (
     <div className="container">
       <header>
         <h1>Gasto Semanal</h1>
+
+        <div className="contenido-principal contenido">
+          <Pregunta />
+        </div>
+        
       </header>
     </div>
   );
