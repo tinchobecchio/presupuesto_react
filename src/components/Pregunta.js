275f7e6d0a3785df86712cb5b2dee0d37806a56c
Martin Becchio <tincho.becchio@gmail.com>
2023-03-28 17:27:21 -0300

    definiendo presupuesto y restante

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Error from "./Error";
 
-const Pregunta = () => {
+const Pregunta = ({ guardarPresupuesto, guardarRestante}) => {
 
     // definir el state
     const [ cantidad, guardarCantidad] = useState(0)
@@ -24,6 +24,8 @@ const Pregunta = () => {
 
         // Si se pasa la validacion
         guardarError(false)
+        guardarPresupuesto(cantidad)
+        guardarRestante(cantidad)
     }
 
     return ( 
