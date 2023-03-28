213f939d6beb045cb81930b74a0ba5c95ba7b4d9
Martin Becchio <tincho.becchio@gmail.com>
2023-03-28 17:17:59 -0300

    creando componente de error

---
+++ src/components/Error.js
@@ -0,0 +1,8 @@
+
+const Error = ({mensaje}) => {
+    return ( 
+        <p className="alert alert-danger error">{mensaje}</p>
+     );
+}
+ 
+export default Error;
--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Error from "./Error";
 
 const Pregunta = () => {
 
@@ -28,6 +29,8 @@ const Pregunta = () => {
     return ( 
         <>
             <h2>Coloca tu presupuesto</h2>
+
+            {error ? <Error mensaje="El Presupuesto es Incorrecto"/> : null}
 
             <form
                 onSubmit={agregarPresupuesto}
